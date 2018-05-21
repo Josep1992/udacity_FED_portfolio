2dc43edd54d3e7f284f7d2fd68ff6e7f69dae0f8
Josep <coding1992@outlook.com>
2018-05-21 00:03:40 -0400

    Initial Commit

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+//Set's the copyright date
+document.querySelector(
+  '.copyright_Date',
+).textContent = new Date().getUTCFullYear();
+
+const current = document.querySelector('.current');
+
+//Images Node List
+const images = document.querySelectorAll('.projects');
+
+const normalSrc = 'images/code.jpg';
+
+const opacity = 0.7;
+
+const changeImageSource = (e) => {
+  current.src = e.target.src;
+  if (e.target.classList.contains('.projects')) {
+    console.log(e.target.src);
+  }
+
+  e.target.style.opacity = opacity;
+};
+
+const changeImageToNormal = () => {
+  current.src = normalSrc;
+};
+
+//On mouse over change the img src
+// images.forEach((img) => img.addEventListener('mouseover', changeImageSource));
+
+//on mouse out return the img src to normal
+// images.forEach((img) => img.addEventListener('mouseout', changeImageToNormal));
