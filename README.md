9a4a011f9ad4e307df4e59df3ccd4a422abe5b5f
Josep <coding1992@outlook.com>
2018-05-21 21:46:50 -0400

    Added Custom Images For the Projects & Changed website Color

---
+++ README.md
@@ -0,0 +1,11 @@
+# Udacity Portfolio MockUp
+
+> This Is the First project in Udacity's Front-End Nanodegree Program(FED)
+
+The Project Rubric is available [here.](https://review.udacity.com/#!/rubrics/45/view)
+
+This project is made with:
+
+1.  HTML
+2.  CSS3
+3.  Flexbox
--- scripts/script.js
+++ scripts/script.js
@@ -3,30 +3,30 @@ document.querySelector(
   '.copyright_Date',
 ).textContent = new Date().getUTCFullYear();
 
-const current = document.querySelector('.current');
+// const current = document.querySelector('.current');
 
-//Images Node List
-const images = document.querySelectorAll('.projects');
+// //Images Node List
+// const images = document.querySelectorAll('.projects');
 
-const normalSrc = 'images/code.jpg';
+// const normalSrc = 'images/header[95].png';
 
-const opacity = 0.7;
+// const opacity = 0.7;
 
-const changeImageSource = (e) => {
-  current.src = e.target.src;
-  if (e.target.classList.contains('.projects')) {
-    console.log(e.target.src);
-  }
+// const changeImageSource = (e) => {
+//   current.src = e.target.src;
+//   if (e.target.classList.contains('.projects')) {
+//     console.log(e.target.src);
+//   }
 
-  e.target.style.opacity = opacity;
-};
+//   e.target.style.opacity = opacity;
+// };
 
-const changeImageToNormal = () => {
-  current.src = normalSrc;
-};
+// const changeImageToNormal = () => {
+//   current.src = normalSrc;
+// };
 
-//On mouse over change the img src
+// // On mouse over change the img src
 // images.forEach((img) => img.addEventListener('mouseover', changeImageSource));
 
-//on mouse out return the img src to normal
+// // on mouse out return the img src to normal
 // images.forEach((img) => img.addEventListener('mouseout', changeImageToNormal));
